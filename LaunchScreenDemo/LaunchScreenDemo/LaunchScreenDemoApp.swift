d86b5b48af4abc6b2107578a3fad5b79f50c5641
Thongchai Subsaidee <chanoktrue@me.com>
2021-10-07 06:56:16 +0700

    up to github

---
+++ LaunchScreenDemo/LaunchScreenDemo/LaunchScreenDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenDemoApp.swift
+//  LaunchScreenDemo
+//
+//  Created by Thongchai Subsaidee on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LaunchScreenDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
